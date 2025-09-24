2ece3b72f402f439ebe161e4202abeb1f16bf337
Alex <alex.gioiosa@gm2dev.com>
2025-09-24 09:55:25 -0300

    ahi va

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-function vehicle(brand, model, maxSpeed) {
+function vehicle(brand, model, maxSpeed, actualSpeed) {
   this.brand = brand;
   this.model = model;
   this.maxSpeed = maxSpeed;
   this.actualSpeed = actualSpeed;
 }
-vehicle.prototype.accelerate = function (increase) {};
+vehicle.prototype.accelerate = function (increase) {
+  this.actualSpeed += increase;
+  if (this.actualSpeed > this.maxSpeed) {
+    this.actualSpeed = this.maxSpeed;
+  }
+  return `Current speed is ${this.actualSpeed}`;
+};
 
-const car1 = new vehicle("BMW", "BMW Serie 2 Gran Coupé", 214);
+const car1 = new vehicle("BMW", "BMW Serie 2 Gran Coupé", 214, 0);
 console.log(car1);
 
-const car2 = new vehicle("Mercedes Benz", "GLE 53", 250);
+const car2 = new vehicle("Mercedes Benz", "GLE 53", 250, 0);
 console.log(car2);
 
-const car3 = new vehicle("Audi", "A5 Sedan", 221);
+const car3 = new vehicle("Audi", "A5 Sedan", 221, 0);
 console.log(car3);
